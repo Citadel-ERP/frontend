054ad23c424f62b9fee857f6135f30ffddd508ca
jayantkhanna1 <jayantkhanna3105@gmail.com>
2025-12-05 19:42:35 +0530

    trying to fix notification error

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,11 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// Block Firebase packages from being bundled
+config.resolver.blockList = [
+  /firebase/,
+  /@react-native-firebase/,
+  /google-services\.json/,
+];
+
 module.exports = config;
