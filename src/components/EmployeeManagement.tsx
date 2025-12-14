aea952f3aa494ac36a66efad99e231406e8d670c
jayantkhanna1 <jayantkhanna3105@gmail.com>
2025-12-14 18:45:23 +0530

    employee management completed

--- src/components/EmployeeManagement.tsx
+++ src/components/EmployeeManagement.tsx
@@ -1118,6 +1118,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     paddingVertical: 16,
     backgroundColor: '#1e1b4b',
+    marginTop: 30,
   },
   backButton: {
     padding: 8,
