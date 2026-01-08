c4c07fa09755b2f4ce6f25b8ad518a36510fe13d
jayantkhanna1 <jayantkhanna3105@gmail.com>
2026-01-08 11:09:47 +0530

    updated design

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,18 +6,18 @@ interface AppConfig {
 // You can also use different configs for different environments
 const configs: Record<string, AppConfig> = {
   development: {
-    // BACKEND_URL: 'https://backend-staging.citadelnetinc.com',
-    // BACKEND_URL_WEBSOCKET: 'https://backend-staging.citadelnetinc.com',
+    BACKEND_URL: 'https://backend-staging.citadelnetinc.com',
+    BACKEND_URL_WEBSOCKET: 'https://backend-staging.citadelnetinc.com',
 
-    BACKEND_URL: 'https://89vlj2ck-8000.inc1.devtunnels.ms/',
-    BACKEND_URL_WEBSOCKET: 'https://89vlj2ck-8000.inc1.devtunnels.ms/',
+    // BACKEND_URL: 'https://89vlj2ck-8000.inc1.devtunnels.ms/',
+    // BACKEND_URL_WEBSOCKET: 'https://89vlj2ck-8000.inc1.devtunnels.ms/',
   },
   production: {
-    // BACKEND_URL: 'https://backend.citadelnetinc.com',
-    // BACKEND_URL_WEBSOCKET: 'https://backend.citadelnetinc.com',
+    BACKEND_URL: 'https://backend.citadelnetinc.com',
+    BACKEND_URL_WEBSOCKET: 'https://backend.citadelnetinc.com',
 
-    BACKEND_URL: 'https://962xzp32-8000.inc1.devtunnels.ms',
-    BACKEND_URL_WEBSOCKET: 'https://962xzp32-8000.inc1.devtunnels.ms',
+    // BACKEND_URL: 'https://962xzp32-8000.inc1.devtunnels.ms',
+    // BACKEND_URL_WEBSOCKET: 'https://962xzp32-8000.inc1.devtunnels.ms',
 
 
   },
